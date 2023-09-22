5eb817969cb1f299f268cd06865bc20a552ac0ac
yacth_Mon <yacthmon@gmail.com>
2023-09-22 18:49:18 +0700

    format: apply formating

--- app.ts
+++ app.ts
@@ -9,15 +9,15 @@ interface ISubscriber {
 }
 
 interface IPublishSubscribeService {
-  publish (event: IEvent): void;
-  subscribe (type: string, handler: ISubscriber): void;
+  publish(event: IEvent): void;
+  subscribe(type: string, handler: ISubscriber): void;
   // unsubscribe ( /* Question 2 - build this feature */ );
 }
 
 
 // implementations
 class MachineSaleEvent implements IEvent {
-  constructor(private readonly _sold: number, private readonly _machineId: string) {}
+  constructor(private readonly _sold: number, private readonly _machineId: string) { }
 
   machineId(): string {
     return this._machineId;
@@ -33,7 +33,7 @@ class MachineSaleEvent implements IEvent {
 }
 
 class MachineRefillEvent implements IEvent {
-  constructor(private readonly _refill: number, private readonly _machineId: string) {}
+  constructor(private readonly _refill: number, private readonly _machineId: string) { }
 
   machineId(): string {
     throw new Error("Method not implemented.");
@@ -47,8 +47,8 @@ class MachineRefillEvent implements IEvent {
 class MachineSaleSubscriber implements ISubscriber {
   public machines: Machine[];
 
-  constructor (machines: Machine[]) {
-    this.machines = machines; 
+  constructor(machines: Machine[]) {
+    this.machines = machines;
   }
 
   handle(event: MachineSaleEvent): void {
@@ -68,7 +68,7 @@ class Machine {
   public stockLevel = 10;
   public id: string;
 
-  constructor (id: string) {
+  constructor(id: string) {
     this.id = id;
   }
 }
@@ -91,7 +91,7 @@ const eventGenerator = (): IEvent => {
   if (random < 0.5) {
     const saleQty = Math.random() < 0.5 ? 1 : 2; // 1 or 2
     return new MachineSaleEvent(saleQty, randomMachine());
-  } 
+  }
   const refillQty = Math.random() < 0.5 ? 3 : 5; // 3 or 5
   return new MachineRefillEvent(refillQty, randomMachine());
 }
@@ -100,7 +100,7 @@ const eventGenerator = (): IEvent => {
 // program
 (async () => {
   // create 3 machines with a quantity of 10 stock
-  const machines: Machine[] = [ new Machine('001'), new Machine('002'), new Machine('003') ];
+  const machines: Machine[] = [new Machine('001'), new Machine('002'), new Machine('003')];
 
   // create a machine sale event subscriber. inject the machines (all subscribers should do this)
   const saleSubscriber = new MachineSaleSubscriber(machines);
@@ -109,7 +109,7 @@ const eventGenerator = (): IEvent => {
   const pubSubService: IPublishSubscribeService = null as unknown as IPublishSubscribeService; // implement and fix this
 
   // create 5 random events
-  const events = [1,2,3,4,5].map(i => eventGenerator());
+  const events = [1, 2, 3, 4, 5].map(i => eventGenerator());
 
   // publish the events
   events.map(pubSubService.publish);
